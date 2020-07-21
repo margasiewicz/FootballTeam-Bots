607e46bb6f51bc1fba47df165e3febc4a51d91b9
Marcin Gasiewicz <margasiewicz@gmail.com>
2020-07-21 16:30:40 +0200

    add pick_parameters method to merket_bot

--- market_bot.py
+++ market_bot.py
@@ -21,6 +21,28 @@ class FTBot:
         sleep(2)
         self.driver.get("https://game.footballteam.pl/market")
         sleep(3)
+    def pick_parameters(self):
+        print('Jaki kolor itemu chcesz kupić?')
+        print("1 - złote\n2 - czerwone\n3 - zielone")
+        print("4 - niebieskie\n5 - szare\n6 - jakikolwiek")
+
+        'TODO: make following more readable'
+        color = int(input())
+        colors = ['gold', 'red', 'green', 'blue', 'grey', 'any']
+        color = colors[color-1]
+
+        print('Po jakiej cenie maksymalnie chcesz kupować?')
+        price = int(input())
+        print('Ile przedmiotów chcesz kupić?')
+        qty = int(input())
+
+        parameters = {
+            'color':color,
+            'price':price,
+            'qty':qty
+        }
+        return parameters
+        
     def market_start(self):
         options = webdriver.ChromeOptions()
         #options.add_argument('--headless')
